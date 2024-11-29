5b6dcc64b577f9b91753f05fa2db4adc5906e1c4
Dat T Nguyen <n.dat@outlook.com>
2024-11-29 13:24:32 +0100

    eval pig

--- eval_pig.sh
+++ eval_pig.sh
@@ -13,7 +13,7 @@ module load Nextflow/24.04.2
 module load singularity/rpm
 
 
-genome='/mnt/users/ngda/genomes/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa'
+#genome='/mnt/users/ngda/genomes/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa'
 
 export NXF_SINGULARITY_CACHEDIR=/mnt/users/ngda/sofware/singularity
 export TOWER_ACCESS_TOKEN=eyJ0aWQiOiA3OTAxfS4xNGY5NTFmOWNiZmEwNjZhOGFkYzliZTg3MDc4YWI4ZTRiYTk4ZmI5
